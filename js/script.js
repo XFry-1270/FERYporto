794785357faebc25f857a7c8ace46090dca347df
XFry <feryralexandria@gmail.com>
2024-01-26 14:58:11 +0700

    new commit change

--- js/script.js
+++ js/script.js
@@ -4,8 +4,23 @@ menu.addEventListener('click',function(){
     menu.classList.toggle('menu-active');
     navMenu.classList.toggle('hidden');
 });
+let section = document.querySelectorAll('section');
+let navink = document.querySelectorAll('nav a');
 
 window.onscroll= function(){
+    section.forEach(sc =>{
+        let top = window.scrollY;
+        let offset = sc.offsetTop - 150;
+        let height = sc.offsetHeight;
+        let id = sc.getAttribute('id');
+        if(top >= offset && top < offset + height){
+            navink.forEach(ink=>{
+                ink.classList.remove('act');
+                document.querySelector('nav a[href*='+ id +']').classList.add('act');
+            })
+        }
+       });
+
     const nav =document.querySelector('header');
     const fixNav = nav.offsetTop;
 
@@ -15,3 +30,6 @@ window.onscroll= function(){
         nav.classList.remove('navbar-fixed');
     }
 }
+
+
+

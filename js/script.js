3112b0752303bb030b53d85874fae32912be3d10
XFry-1270 <feryralexandria@gmail.com>
2024-02-04 16:28:55 +0700

    Commit darkmode

--- js/script.js
+++ js/script.js
@@ -29,7 +29,72 @@ window.onscroll= function(){
     }else{
         nav.classList.remove('navbar-fixed');
     }
+    
+}
+
+// DARKMODE PART START
+
+var dmbtn = document.getElementById('dm');
+
+dmbtn.addEventListener('click',darkmode);
+
+function darkmode(){
+    var body = document.querySelector("body");
+    var abouth = document.querySelectorAll('#About h2');
+    var aboutspan = document.querySelectorAll('#About span');
+    var homespan = document.querySelectorAll('#Home span');
+    var porto = document.querySelector("#Portofolio")
+    var portoH = document.querySelectorAll('#Portofolio h2');
+    var portoP = document.querySelectorAll('#Portofolio p');
+
+    body.classList.toggle("darkmode-body");
+
+
+    if(abouth[0].classList.contains('darkmode-txt')){
+        dmbtn.src='asset/moon.svg';
+        abouth[0].classList.remove('darkmode-txt');
+        abouth[1].classList.remove('darkmode-txt');
+        aboutspan[0].classList.remove('darkmode-txt');
+        homespan[2].classList.remove('darkmode-txt');
+        homespan[3].classList.remove('darkmode-txt');
+        porto.classList.remove('darkmode-portofolio');
+
+        portoH[0].classList.remove('darkmode-txt');
+        portoH[1].classList.remove('darkmode-txt');
+        portoH[2].classList.remove('darkmode-txt');
+        portoH[3].classList.remove('darkmode-txt');
+        portoH[4].classList.remove('darkmode-txt');
+
+        portoP[0].classList.remove('darkmode-p');
+        portoP[1].classList.remove('darkmode-p');
+        portoP[2].classList.remove('darkmode-p');
+        portoP[3].classList.remove('darkmode-p');
+        portoP[4].classList.remove('darkmode-p');
+
+    }else{
+    abouth[0].classList.add('darkmode-txt');
+    abouth[1].classList.add('darkmode-txt');
+    aboutspan[0].classList.add('darkmode-txt');
+    homespan[2].classList.add('darkmode-txt');
+    homespan[3].classList.add('darkmode-txt');
+    porto.classList.add('darkmode-portofolio');
+
+    portoH[0].classList.add('darkmode-txt');
+    portoH[1].classList.add('darkmode-txt');
+    portoH[2].classList.add('darkmode-txt');
+    portoH[3].classList.add('darkmode-txt');
+    portoH[4].classList.add('darkmode-txt');
+
+    portoP[0].classList.add('darkmode-p');
+    portoP[1].classList.add('darkmode-p');
+    portoP[2].classList.add('darkmode-p');
+    portoP[3].classList.add('darkmode-p');
+    portoP[4].classList.add('darkmode-p');
+    dmbtn.src="asset/sun.svg";
+    }
+
+    
 }
 
 
-
+// DARKMODE PART END 
